bbf720a626d3ce7c088a670fb7ea4571d7cf163e
Michał Wiaderkowicz <michal.wiaderkowicz@onet.pl>
2023-11-07 21:14:34 +0100

    add render

--- src/js/script.js
+++ src/js/script.js
@@ -8,14 +8,37 @@
 
   console.log('thisApp: window?', thisApp);
 
-  /* Prepare a reference to the template and the .books-list. */
   getElem.dom = {};
 
+  /* Prepare a reference to the template and the .books-list. */
   getElem.dom.template = document.querySelector('#template-book');
   getElem.dom.booksList = document.querySelector('.books-list');
 
-  console.log('element DOM Szablon:', getElem.dom.template);
-  console.log('element DOM .book-list:', getElem.dom.booksList);
+  const template = Handlebars.compile(document.querySelector('#template-book').innerHTML);
+  console.log('template:', template);
 
+  const library = dataSource.books;
+  console.log('library:', library);
+
+  /* Add a new render function */
+  const render = function(book) {
+    /* START LOOP AND step through each item from dataSource.books */
+    for(let data of book){ 
+      console.log('data', data);
+
+      /* generating HTML code based on the template and data about a specific book. */
+      const generatedHTML = template(data);
+      console.log('generatedHTML:', generatedHTML);
+
+      /* Generate a DOM element based on this (LOOK UP) HTML code. */
+      const generatedDOM = utils.createDOMFromHTML(generatedHTML);
+      console.log('generatedDOM:', generatedDOM);
+
+      /* Append the generated DOM element as a new child DOM to the .books-list */
+      getElem.dom.booksList.appendChild(generatedDOM);
+    }
+  };
+
+  render(library);
 
 }
